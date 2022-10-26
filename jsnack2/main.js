af8da585101201503f77308cffd9981e7f3fdde1
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-26 10:56:32 +0200

    Aggiunge soluzione

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,2 +1,14 @@
 // L’utente inserisce due parole in successione, con due prompt.
+
+const firstWord = prompt("Inserisci la prima parola");
+
+const secondWord = prompt("Inserisci la seconda parola");
+
+
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+if ( firstWord.length > secondWord.length ) {
+    console.log(secondWord,firstWord);
+} else {
+    console.log(firstWord, secondWord);
+}
