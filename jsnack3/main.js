7152e7a421d04285d98be4bff55abcc5d2acf35a
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-26 11:49:52 +0200

    Soluzione

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,10 +1,11 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
+let add = 0;
 
 for ( i=1; i <=10; i++) {
     const number =  Number( prompt("Inserisci un numero"));
 
-    const add = number + number + number + number + number + number + number + number + number + number;
+    add= add + number;
 
-    // Il programma stampa la somma di tutti i numeri inseriti.
-    console.log(add);
 }
+// Il programma stampa la somma di tutti i numeri inseriti.
+console.log(add);
